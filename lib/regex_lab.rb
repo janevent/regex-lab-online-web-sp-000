4308d04747a122aef883ac75e9ea7552985bc8ff
Jane Venter <janeanneventer@gmail.com>
2020-04-11 18:24:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,24 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  #binding.pry
+  if word[0].match(/[aeiouAEIOU]/)
+    return true 
+  else 
+    return false 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  #binding.pry
+  text.scan(/\w+ing/)
 
 end
 
 def words_five_letters_long(text)
-
+  #binding.pry
+  text.scan(/\w+{5,5}/)
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
